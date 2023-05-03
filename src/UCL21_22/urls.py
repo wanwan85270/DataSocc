977d1dba7138ffa5756b19fe6cdbacf4fdf364e0
wanwan85270 <max.michon@live.fr>
2023-05-03 21:55:27 +0200

    feat: bientot fini mais pas pret

--- src/UCL21_22/urls.py
+++ src/UCL21_22/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import UCL21_22, club_joueurs, joueur_stats, stats_detail
+from .views import UCL21_22, club_joueurs, club_joueurs_B, joueur_stats, joueur_stats_B, stats_detail, stats_detail_B, stats_detail_S
 
 urlpatterns = [
     path('', UCL21_22, name="UCL21_22"),
     path('club_joueurs/', club_joueurs, name='club_joueurs'),
-    path('joueur_stats/', joueur_stats, name='joueur_stats'),  
-    path('stats_detail/', stats_detail, name='stats_detail')
+    path('club_joueurs_B/', club_joueurs_B, name='club_joueurs_B'),
+    path('joueur_stats/', joueur_stats, name='joueur_stats'),
+    path('joueur_stats_B/', joueur_stats_B, name='joueur_stats_B'),  
+    path('stats_detail/', stats_detail, name='stats_detail'),
+    path('stats_detail_B/', stats_detail_B, name='stats_detail_B'),
+    path('stats_detail_S/', stats_detail_S, name='stats_detail_S')
 ]
--- src/UCL21_22/views.py
+++ src/UCL21_22/views.py
@@ -5,6 +5,7 @@ import csv
 from .models import modelUCL
 from matplotlib import pyplot as plt
 import math
+import numpy as np
 
 
 
@@ -66,7 +67,7 @@ def BDDUCL21_22(request):
 def UCL21_22(request):
     date = datetime.today()
     clubs = modelUCL.objects.values_list("club", flat=True).distinct()
-    context = {"date": date, "clubs": clubs}
+    context = {"date": date, "clubs": clubs, "clubs_B":clubs}
     template_name = "club_list.html"
     return render(request, template_name, context=context)
     
@@ -80,6 +81,15 @@ def club_joueurs(request):
     template_name = "joueur_list.html"
     return render(request, template_name, context=context)
 
+def club_joueurs_B(request):
+    club_B = request.GET.get('club_B')
+    print(club_B)
+    joueurs_B = modelUCL.objects.filter(club=club_B)
+    print(joueurs_B)
+    context = {'joueurs_B': joueurs_B}
+    template_name = "joueur_B_list.html"
+    return render(request, template_name, context=context)
+
 def joueur_stats(request):
     joueur_id = request.GET.get("joueur")
     joueur = get_object_or_404(modelUCL, pk=joueur_id)
@@ -91,14 +101,26 @@ def joueur_stats(request):
     template_name = "statistiques_list.html"
     return render(request, template_name, context=context)
 
+def joueur_stats_B(request):
+    joueur_id_B = request.GET.get("joueur_B")
+    joueur_B = get_object_or_404(modelUCL, pk=joueur_id_B)
+    joueur_stats_B = modelUCL.objects.filter(id=joueur_id_B)
+    print(joueur_id_B)
+    print(joueur_B)
+    print(joueur_stats_B)
+    context = {"joueur_B": joueur_B, "joueur_stats_B": joueur_stats_B}
+    template_name = "statistiques_list_B.html"
+    return render(request, template_name, context=context)
+
 def stats_detail(request):
+    #on défini les variables
     stats = request.GET.getlist('stats')
     joueur_id = request.GET.get('joueur_id')
     joueur = get_object_or_404(modelUCL, pk=joueur_id)
     joueur_stats = []
     for stat in stats:
         joueur_stats.append(getattr(joueur, stat))
-    print (stats)
+    print(stats)
     print(joueur_id)
     print(joueur)
     print(joueur_stats)
@@ -121,12 +143,16 @@ def stats_detail(request):
 
     normalized_stats_values = [val/max_val for val, max_val in zip(stats_values, max_values)]
 
+    #Sur quels angles les statistiques s'affichent sur le diagramme
     angles = [n / float(len(stats_names)) * 2 * math.pi for n in range(len(stats_names))] 
     print(angles)
 
+    #Initialise le diagramme
     ax = plt.subplot(111, polar=True)
+    #Pour avoir la première stat au sommet
     ax.set_theta_offset(math.pi / 2)
     ax.set_theta_direction(-1)
+    #Dessine un axe par stat et les étiquettes
     plt.xticks(angles, stats_names)
     ax.plot(angles, normalized_stats_values, linewidth=1, linestyle='solid')
     ax.fill(angles, normalized_stats_values, 'blue', alpha=0.1)
@@ -137,6 +163,121 @@ def stats_detail(request):
 
     context = {'joueur': joueur, 'stats': stats, 'joueur_stats_dict': joueur_stats_dict}
     template_name = 'joueur_detail.html'
+
+    return render(request, template_name, context=context)
+
+
+def stats_detail_B(request):
+    stats_B = request.GET.getlist('stats_B')
+    joueur_id_B = request.GET.get('joueur_id_B')
+    joueur_B = get_object_or_404(modelUCL, pk=joueur_id_B)
+    joueur_stats_B = []
+    for stat in stats_B:
+        joueur_stats_B.append(getattr(joueur_B, stat))
+    print (stats_B)
+    print(joueur_id_B)
+    print(joueur_B)
+    print(joueur_stats_B)
+
+    joueur_stats_dict_B = {}
+    for i in range(len(stats_B)):
+        joueur_stats_dict_B[stats_B[i]] = joueur_stats_B[i]
+    print(joueur_stats_dict_B)
+
+    stats_names_B = list(joueur_stats_dict_B.keys())
+    stats_values_B = list(joueur_stats_dict_B.values())
+    print(stats_names_B)
+    print(stats_values_B)
+
+    max_values_B = []
+    for stat in stats_B:
+        max_value_B = modelUCL.objects.aggregate(Max(stat))[stat + '__max']
+        max_values_B.append(max_value_B)    
+        print (max_values_B)
+
+    normalized_stats_values_B = [val/max_val for val, max_val in zip(stats_values_B, max_values_B)]
+    print(normalized_stats_values_B)
+
+    angles_B = [n / float(len(stats_names_B)) * 2 * math.pi for n in range(len(stats_names_B))] 
+    print(angles_B)
+
+    ax = plt.subplot(111, polar=True)
+    ax.set_theta_offset(math.pi / 2)
+    ax.set_theta_direction(-1)
+    plt.xticks(angles_B, stats_names_B)
+    ax.plot(angles_B, normalized_stats_values_B, 'red', linewidth=1, linestyle='solid')
+    ax.fill(angles_B, normalized_stats_values_B, 'red', alpha=0.3)
+
+    plt.ylim(0, 1.0)
+    plt.title(joueur_B.nom_joueur + " - " + joueur_B.club)
+    plt.show()
+
+    context = {'joueur': joueur_B, 'stats': stats_B,'joueur_stats_dict_B': joueur_stats_dict_B}
+    template_name = 'joueur_detail.html'
+
+    return render(request, template_name, context=context)
+
+def stats_detail_S(request):
+    joueur_id_S = request.GET.get('joueur_id_S')
+    joueur_S = get_object_or_404(modelUCL, pk=joueur_id_S)
+    print(joueur_id_S)
+    print(joueur_S)
+    
+    stats_B_S = request.GET.getlist('stats_B')
+    print(stats_B_S)
+    joueur_id_B_S = request.GET.get('joueur_id_B')
+    joueur_B_S = get_object_or_404(modelUCL, pk=joueur_id_B_S)   
+    print(joueur_B_S)
+    print(joueur_id_B_S) 
+    
+    joueur_stats_S = []
+    joueur_stats_B_S = []
+    for stat in stats_B_S:
+        joueur_stats_S.append(getattr(joueur_S, stat))
+        joueur_stats_B_S.append(getattr(joueur_B_S, stat))
+        
+    joueur_stats_dict_S = {}
+    joueur_stats_dict_B_S = {}
+    for i in range(len(stats_B_S)):
+        joueur_stats_dict_S[stats_B_S[i]] = joueur_stats_S[i]
+        joueur_stats_dict_B_S[stats_B_S[i]] = joueur_stats_B_S[i]
+
+    stats_names_S = list(joueur_stats_dict_S.keys())
+    stats_values_S = list(joueur_stats_dict_S.values())
+    stats_values_B_S = list(joueur_stats_dict_B_S.values())
+
+    max_values_S = []
+    max_values_B_S = []
+    for stat in stats_B_S:
+        max_value_S = modelUCL.objects.aggregate(Max(stat))[stat + '__max']
+        max_value_B_S = max_value_S
+        max_values_S.append(max_value_S)
+        max_values_B_S.append(max_value_B_S)
+
+    normalized_stats_values_S = [val/max_val for val, max_val in zip(stats_values_S, max_values_S)]
+    normalized_stats_values_B_S = [val/max_val for val, max_val in zip(stats_values_B_S, max_values_B_S)]
+
+    angles = [n / float(len(stats_names_S)) * 2 * math.pi for n in range(len(stats_names_S))] 
+
+    ax = plt.subplot(111, polar=True)
+    ax.set_theta_offset(math.pi / 2)
+    ax.set_theta_direction(-1)
+    plt.xticks(angles, stats_names_S)
+
+    ax.plot(angles, normalized_stats_values_S, linewidth=1, linestyle='solid', color='blue', label=joueur_S.nom_joueur)
+    ax.fill(angles, normalized_stats_values_S, 'blue', alpha=0.1)
+    ax.plot(angles, normalized_stats_values_B_S, 'red', linewidth=1, linestyle='solid', label=joueur_B_S.nom_joueur)
+    ax.fill(angles, normalized_stats_values_B_S, 'red', alpha=0.3)
+
+    plt.ylim(0, 1.0)
+    plt.title(joueur_S.nom_joueur + " - " + joueur_S.club + " vs " + joueur_B_S.nom_joueur + " - " + joueur_B_S.club)
+    plt.legend(loc='upper right')
+    plt.show()
+
+
+    context = {'joueur': joueur_S, 'stats': stats_B_S, 'joueur_stats_dict': joueur_stats_dict_S,
+               'joueur_B': joueur_B_S, 'joueur_stats_dict_B': joueur_stats_dict_B_S}
+    template_name = 'diagram.html'
 
     return render(request, template_name, context=context)
 
